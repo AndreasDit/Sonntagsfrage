d3139400701144d61a0c52c7a79fbcedf9d63c05
Andreas Ditte <Andreas.Ditte01@gmail.com>
2021-02-14 19:18:19 +0100

    WiP commit. Start delting env file

--- src/Fctn_Orchestrator_Pipeline/__init__.py
+++ src/Fctn_Orchestrator_Pipeline/__init__.py
@@ -8,18 +8,38 @@
 
 import logging
 import json
-
+from datetime import datetime, timedelta
 import azure.functions as func
 import azure.durable_functions as df
 
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
-    result1 = yield context.call_activity('crawl_data_questionaire_results_activity')
-    result2 = yield context.call_activity('clean_crawled_data_activity')
-    result3 = yield context.call_activity('forecaster_activity')
-    result4 = yield context.call_activity('google_spreadsheets_activity')
+    # --- define parameters
+    pipeline_run_from_starter = None
+    aml_pipeline_starter = {"pipeline_name": "Sonntagsfrage-Forecaster-Pipeline",
+                            "workspace_name": "Sonntagsfrage-predictor"}
+
+    # --- start data scraping
+    # result1 = yield context.call_activity('crawl_data_questionaire_results_activity')
+
+    # --- start data cleaning
+    # result2 = yield context.call_activity('clean_crawled_data_activity')
+
+    # --- start predicting values via the Azure ML Service
+    result3, pipeline_run_from_starter = yield context.call_activity('sonntagspredictor_aml_pipeline_starter_activity', aml_pipeline_starter)
+    deadline = context.current_utc_datetime + timedelta(minutes=10)
+    yield context.create_timer(deadline)
+    aml_pipeline_checker = {"pipeline_name": "Sonntagsfrage-Forecaster-Pipeline",
+                            "workspace_name": "Sonntagsfrage-predictor",
+                            "pipeline_run": pipeline_run_from_starter}
+    result4 = yield context.call_activity('sonntagspredictor_aml_pipeline_starter_activity', aml_pipeline_checker)
+
+    # --- start transferring data to the google sheet service
+    # result5 = yield context.call_activity('google_spreadsheets_activity')
+
     return [
-        result1, 
-        result2, result3, result4]
+        result1,
+        result2, result3, result4, result5]
+
 
 main = df.Orchestrator.create(orchestrator_function)
--- src/sonntagspredictor_aml_pipeline_starter_activity/__init__.py
+++ src/sonntagspredictor_aml_pipeline_starter_activity/__init__.py
@@ -7,11 +7,41 @@
 # - run pip install -r requirements.txt
 
 import logging
+import sys
+from azureml.core import Experiment, Workspace, Environment
+from azureml.pipeline.core import Pipeline, PipelineData, PipelineEndpoint
+from environs import Env
 
 
-def main(pipeline_name: str) -> str:
-    print(f"Triggering pipeline endpoint '{pipeline_name}' (as configured)...")
+def main(pipelineName) -> str:
+    # --- read params
+    pipelineName = "Sonntagsfrage-Forecaster-Pipeline"
+    workspaceName = "Sonntagsfrage-predictor"
+
+    # --- load configuration
+    print("Loading configuration...")
+    for p in sys.path:
+        print(p)
+    env = Env()
+    env.read_env("Azure_ML/foundation.env")
+
+    azure_subscription_id = env("AZURE_SUBSCRIPTION_ID")
+    resource_group = env("RESOURCE_GROUP")
+    workspaceName = env("workspaceName")
+
+    # --- get workspace, compute target, run config
+    print("Getting workspace and compute target...")
+    workspace = Workspace(
+        subscription_id=azure_subscription_id,
+        resource_group=resource_group,
+        workspaceName=workspaceName,
+    )
+
+    print(f"Get pipeline endpoint '{pipelineName}' (as configured)...")
+    pipeline_endpoint = PipelineEndpoint.get(
+        workspace=workspace, name=pipelineName)
+
+    print(f"Triggering pipeline endpoint '{pipelineName}' (as configured)...")
     pipeline_run = Experiment(
-        workspace, pipeline_name).submit(pipeline_endpoint)
-    pipeline_run.wait_for_completion()
-    return f"Pipeline {pipeline_name} completed successfully!"
+        workspace, pipelineName).submit(pipeline_endpoint)
+    return f"Pipeline {pipelineName} has been successfully started!", pipeline_run
