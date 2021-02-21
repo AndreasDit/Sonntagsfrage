835db77c588620715f99627a04cb758dac9f86e3
Andreas Ditte <Andreas.Ditte01@gmail.com>
2021-02-21 19:08:50 +0100

    Smaller Fixes

--- src/sonntagspredictor_aml_pipeline_starter_activity/__init__.py
+++ src/sonntagspredictor_aml_pipeline_starter_activity/__init__.py
@@ -8,40 +8,11 @@
 
 import logging
 import sys
-from azureml.core import Experiment, Workspace, Environment
-from azureml.pipeline.core import Pipeline, PipelineData, PipelineEndpoint
-from environs import Env
+import Azure_ML.aml_pipeline_for_durable as pipeline
 
 
 def main(pipelineName) -> str:
-    # --- read params
-    pipelineName = "Sonntagsfrage-Forecaster-Pipeline"
-    workspaceName = "Sonntagsfrage-predictor"
-
-    # --- load configuration
-    print("Loading configuration...")
-    for p in sys.path:
-        print(p)
-    env = Env()
-    env.read_env("Azure_ML/foundation.env")
-
-    azure_subscription_id = env("AZURE_SUBSCRIPTION_ID")
-    resource_group = env("RESOURCE_GROUP")
-    workspaceName = env("workspaceName")
-
-    # --- get workspace, compute target, run config
-    print("Getting workspace and compute target...")
-    workspace = Workspace(
-        subscription_id=azure_subscription_id,
-        resource_group=resource_group,
-        workspaceName=workspaceName,
-    )
-
-    print(f"Get pipeline endpoint '{pipelineName}' (as configured)...")
-    pipeline_endpoint = PipelineEndpoint.get(
-        workspace=workspace, name=pipelineName)
-
     print(f"Triggering pipeline endpoint '{pipelineName}' (as configured)...")
-    pipeline_run = Experiment(
-        workspace, pipelineName).submit(pipeline_endpoint)
+    msg, pipeline_run = pipeline.start_sonntagsfrage_pipeline()
+
     return f"Pipeline {pipelineName} has been successfully started!", pipeline_run
