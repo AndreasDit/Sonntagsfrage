3e6af0c52f35730273b1dffb74bda6948b69e529
Andreas Ditte <Andreas.Ditte01@gmail.com>
2021-03-03 19:12:59 +0100

    Add Metrics and Algos to google Data for Tableau

--- src/google_spreadsheets/load_data_spreadsheet.py
+++ src/google_spreadsheets/load_data_spreadsheet.py
@@ -1,6 +1,3 @@
-import utils.connectivity as conns
-import utils.configs_for_code as cfg
-import utils.logs as logs
 import gspread
 import yaml
 import os
@@ -8,6 +5,9 @@ import sys
 import pandas as pd
 
 sys.path.append(os.getcwd())
+import utils.connectivity as conns
+import utils.configs_for_code as cfg
+import utils.logs as logs
 
 
 configs_file = open(cfg.PATH_CONFIG_FILE, 'r')
@@ -16,8 +16,11 @@ logger = logs.create_logger(__name__)
 
 DATA_SPREADSHEET_NAME = configs['google']['data_spreadsheet_name']
 PREDS_SPREADSHEET_NAME = configs['google']['preds_spreadsheet_name']
+METRICS_SPREADSHEET_NAME = configs['google']['metrics_spreadsheet_name']
+
 DATA_WORKSHEET_NAME = configs['google']['data_worksheet_name']
 PREDS_WORKSHEET_NAME = configs['google']['preds_worksheet_name']
+METRICS_WORKSHEET_NAME = configs['google']['metrics_worksheet_name']
 
 
 def empty_worksheet(worksheet):
@@ -72,23 +75,31 @@ def main():
     conn_azure, cursor_azure = conns.connect_to_azure_sql_db()
     conn_google = conns.connect_to_google_spreadsheets()
 
-    # load worksheets
+    # load sheets
     sheet_data = conn_google.open(DATA_SPREADSHEET_NAME)
     sheet_preds = conn_google.open(PREDS_SPREADSHEET_NAME)
+    sheet_metrics =  conn_google.open(METRICS_SPREADSHEET_NAME)
+
+    # load worksheets
     data_worksheet = sheet_data.worksheet(DATA_WORKSHEET_NAME)
     preds_worksheet = sheet_preds.worksheet(PREDS_WORKSHEET_NAME)
+    metrics_worksheet = sheet_metrics.worksheet(METRICS_WORKSHEET_NAME)
 
     # load tables from Azure SQL DB
     sql_stmt = """select * from sonntagsfrage.v_predictions_questionaire_pivot"""
     df_table_preds = pd.read_sql(sql_stmt, conn_azure)
     sql_stmt = """select * from sonntagsfrage.v_results_questionaire_clean_pivot"""
     df_table_data = pd.read_sql(sql_stmt, conn_azure)
+    sql_stmt = """select * from sonntagsfrage.metric_results"""
+    df_table_metrics = pd.read_sql(sql_stmt, conn_azure)
 
     empty_worksheet(data_worksheet)
     empty_worksheet(preds_worksheet)
+    empty_worksheet(metrics_worksheet)
 
     fill_worksheet_from_df(data_worksheet, df_table_data)
     fill_worksheet_from_df(preds_worksheet, df_table_preds)
+    fill_worksheet_from_df(metrics_worksheet, df_table_metrics)
 
 
 if __name__ == "__main__":
