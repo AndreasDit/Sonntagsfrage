0b813d4c167614da9ddbbc3997619786c6175c41
Andreas Ditte <Andreas.Ditte01@gmail.com>
2021-05-09 13:34:13 +0200

    Add scrtipt to deploy pipeline.

--- src/Azure_ML/deploy_pipeline.sh
+++ src/Azure_ML/deploy_pipeline.sh
@@ -1,11 +1,2 @@
 # Execute this script with . ./deploy_pipeline.sh !!!
-cd ../
-mv .gitignore .gitignoreorig
-mv .gitignoredeploy .gitignore
-
 /Users/andreasditte/Desktop/Private_Projekte/Sonntagsfrage/src/.venv/bin/python /Users/andreasditte/Desktop/Private_Projekte/Sonntagsfrage/src/Azure_ML/03_pipeline/setup.py --no-trigger-after-publish --no-schedule
-
-mv .gitignore .gitignoredeploy
-mv .gitignoreorig .gitignore
-
-cd Azure_ML/
